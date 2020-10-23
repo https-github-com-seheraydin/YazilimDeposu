69d5f03deca3b086222853b33507b7625af23761
seheraydin <seheraydin056@gmail.com>
2020-10-23 03:39:01 +0300

    tema entegrasyonu

---
+++ web_projesi/web_projesi/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_projesi.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
