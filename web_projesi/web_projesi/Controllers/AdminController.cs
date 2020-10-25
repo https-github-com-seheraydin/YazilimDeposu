9359adc8324c478c38c67342aee597360c5ae33e
seheraydin <seheraydin056@gmail.com>
2020-10-25 16:44:21 +0300

    Database Connect

--- web_projesi/web_projesi/Controllers/AdminController.cs
+++ web_projesi/web_projesi/Controllers/AdminController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web_projesi.Models;
 
 namespace web_projesi.Controllers
 {
     public class AdminController : Controller
     {
+        //veri tabanındaki tablolara erişmek için
+        YazilimDeposuDBEntities db = new YazilimDeposuDBEntities();
+
+
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            //kategorileri liste şeklinde gösteren bir sorgu oluşturalım
+            var sorgu = db.Kategoris.ToList();
+            return View(sorgu);
         }
     }
 }
