50df71b76294609c56a605e93a21f73f880c463c
seheraydin <seheraydin056@gmail.com>
2020-10-31 19:38:18 +0300

    Hakkımızda açıklama güncellemesi-CKEditor eklenmesi

--- web_projesi/web_projesi/Controllers/HakkimizdaController.cs
+++ web_projesi/web_projesi/Controllers/HakkimizdaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using web_projesi.Models.DataContext;
+using web_projesi.Models.Model;
 
 namespace web_projesi.Controllers
 {
@@ -14,6 +15,28 @@ namespace web_projesi.Controllers
         public ActionResult Index()
         {
             var h = db.Hakkimizda.ToList();
+            return View(h);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var h = db.Hakkimizda.Where(x => x.HakkimizdaId == id).FirstOrDefault();
+            return View(h);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(int id, Hakkimizda h)
+        {
+            if (ModelState.IsValid)
+            {
+                var hakkimida = db.Hakkimizda.Where(x => x.HakkimizdaId == id).SingleOrDefault();
+
+                hakkimida.Aciklama = h.Aciklama;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
             return View(h);
         }
     }
