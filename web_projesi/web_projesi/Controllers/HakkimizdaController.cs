5673d40045a3b9d54794e96f2a2ed2d59aa258cd
seheraydin <seheraydin056@gmail.com>
2020-10-31 18:38:15 +0300

    Hakkımızda linki & DB den verilerin listelenmesi

---
+++ web_projesi/web_projesi/Controllers/HakkimizdaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_projesi.Models.DataContext;
+
+namespace web_projesi.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        YazilimDeposuDBContext db = new YazilimDeposuDBContext();
+        // GET: Hakkimizda
+        public ActionResult Index()
+        {
+            var h = db.Hakkimizda.ToList();
+            return View(h);
+        }
+    }
+}
