8ea46f994beeee43655354392d665232d2e2860c
seheraydin <seheraydin056@gmail.com>
2020-11-02 19:10:19 +0300

    İletişi Formu işlemleri Yeni-Güncelle-Detay-Görüntüle-Sil

---
+++ web_projesi/web_projesi/Controllers/HizmetController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_projesi.Models.DataContext;
+
+namespace web_projesi.Controllers
+{
+    public class HizmetController : Controller
+    {
+        private YazilimDeposuDBContext db = new YazilimDeposuDBContext();
+        // GET: Hizmet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
