789b86491f62221aee53fd047f523b8fdbcca9e3
seheraydin <seheraydin056@gmail.com>
2020-11-06 21:38:13 +0300

    Blog Ekleme İşlemleri

--- web_projesi/web_projesi/Controllers/BlogController.cs
+++ web_projesi/web_projesi/Controllers/BlogController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using web_projesi.Models.DataContext;
 using web_projesi.Models.Model;
@@ -23,6 +25,27 @@ namespace web_projesi.Controllers
         {
             ViewBag.KategoriId = new SelectList(db.Kategori, "KategoriId", "KategoriAd");
             return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Blog blog, HttpPostedFileBase ResimURL)
+        {
+            if (ResimURL != null)
+            {
+               
+                WebImage img = new WebImage(ResimURL.InputStream);
+                FileInfo imginfo = new FileInfo(ResimURL.FileName);
+
+                String blogImgName = Guid.NewGuid().ToString() + imginfo.Extension;
+                img.Resize(600, 400);
+                img.Save("~/Uploads/Blog/" + blogImgName);
+
+                blog.ResimURL = "/Uploads/Blog/" + blogImgName;
+
+            }
+            db.Blog.Add(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
