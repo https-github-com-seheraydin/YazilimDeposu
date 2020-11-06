c0a2ad2619a6ca8a968b375f9e6f7e8a922519cb
seheraydin <seheraydin056@gmail.com>
2020-11-06 15:37:52 +0300

    Blog Kayıtları Listeleme

---
+++ web_projesi/web_projesi/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web_projesi.Models.DataContext;
+using web_projesi.Models.Model;
+
+namespace web_projesi.Controllers
+{
+    public class BlogController : Controller
+    {
+        private YazilimDeposuDBContext db = new YazilimDeposuDBContext();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var b = db.Blog.ToList();
+            return View(b);
+        }
+    }
+}
