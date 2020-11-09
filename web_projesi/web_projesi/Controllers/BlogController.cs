f2ef8de5d65c6a49bf6d49cf28f64fc54c84c9ea
seheraydin <seheraydin056@gmail.com>
2020-11-09 14:21:21 +0300

    Blog Kayıt Güncelleme

--- web_projesi/web_projesi/Controllers/BlogController.cs
+++ web_projesi/web_projesi/Controllers/BlogController.cs
@@ -28,6 +28,7 @@ namespace web_projesi.Controllers
         }
         [HttpPost]
         [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(Blog blog, HttpPostedFileBase ResimURL)
         {
             if (ResimURL != null)
@@ -46,6 +47,53 @@ namespace web_projesi.Controllers
             db.Blog.Add(blog);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            if (id==null)
+            {
+                return HttpNotFound();
+            }
+            var b = db.Blog.Where(x => x.BlogId == id).SingleOrDefault();
+            if (b==null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.KategoriId = new SelectList(db.Kategori, "KategoriId", "KategoriAd", b.KategoriId);
+            return View(b);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(int id, Blog blog, HttpPostedFileBase ResimURL)
+        {
+            if (ModelState.IsValid)
+            {
+                var b = db.Blog.Where(x => x.BlogId == id).SingleOrDefault();
+                if (ResimURL != null)
+                {
+                    //daha önce kaydetmiş olunan dosya kontrolü
+                    if (System.IO.File.Exists(Server.MapPath(b.ResimURL)))
+                    {
+                        System.IO.File.Delete(Server.MapPath(b.ResimURL));
+                    }
+
+                    WebImage img = new WebImage(ResimURL.InputStream);
+                    FileInfo imginfo = new FileInfo(ResimURL.FileName);
+
+                    String blogimgname = Guid.NewGuid().ToString() + imginfo.Extension;
+                    img.Resize(500, 500);
+                    img.Save("~/Uploads/Blog/" + blogimgname);
+
+                    b.ResimURL = "/Uploads/Blog/" + blogimgname;
+                }
+                b.Baslik = blog.Baslik;
+                b.Icerik = blog.Icerik;
+                b.KategoriId = blog.KategoriId;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blog);
         }
     }
 }
